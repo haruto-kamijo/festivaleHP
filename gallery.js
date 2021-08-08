f6d6a9932a72114685d0bb40efe997f1fec69fe2
Ark-00 <pon_929_2002@yahoo.co.jp>
2021-08-08 22:20:07 +0900

    first commit

---
+++ gallery.js
@@ -0,0 +1,13 @@
+var pics_src=new Array("pics/1.jpg","pics/2.jpg","pics/3.jpg","pics/4.jpg","pics/5.jpg","pics/6.jpg","pics/7.jpg","pics/8.jpg","pics/9.jpg");
+var num=0;
+
+function slideshow(){
+  if(num==8){
+    num=0;
+  }
+  else{
+    num++;
+  }
+  document.getElementById("mypic").src=pics_src[num];
+  setTimeout("slideshow()",5000);
+}
